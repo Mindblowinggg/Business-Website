9e076d545abc46a146e629a8929f03f688da9e7b
Mindblowinggg <akahar36@gmail.com>
2025-11-08 14:04:44 +0530

    sdas

--- src/components/teaminfo.jsx
+++ src/components/teaminfo.jsx
@@ -1,25 +1,13 @@
 import React, { useState } from "react";
 import { teamMembers } from "../utils/constants";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Teaminfo = () => {
-  const [currentindex, setcurrentindex] = useState(0);
-  const totalmembers = teamMembers.length;
-
-  const prevbtn = () => {
-    const isfirstindex = currentindex === 0;
-    const newindex = isfirstindex ? totalmembers - 1 : currentindex - 1;
-    setcurrentindex(newindex);
-  };
-
-  const nextbtn = () => {
-    const islastindex = currentindex === totalmembers - 1;
-    const newindex = islastindex ? 0 : currentindex + 1;
-    setcurrentindex(newindex);
-  };
-
-  const currentmember = teamMembers[currentindex];
-
   return (
     <div className=" relative flex flex-col justify-center items-center text-center py-10">
       <p className="text-[#74C69D] text-[16px] tracking-widest select-none font-bold sm:text-lg ">
@@ -29,27 +17,51 @@ const Teaminfo = () => {
         Meet Our Experts
       </p>
       <p className="select-none pt-3 pb-5">
-        Behind every successful solution is a team of dedicated professionals. <br /> Discover the expertise driving our innovation.
+        Behind every successful solution is a team of dedicated professionals.{" "}
+        <br /> Discover the expertise driving our innovation.
       </p>
 
-      {/* card */}
-      <div className="relative w-full max-w-[400px] mb-35 sm:mb-35 md:mb-20 min-w-[150px] bg-[#74c69dae] flex justify-center items-center">
-        <div className="flex flex-col justify-center gap-3 items-center text-2xl font-medium w-full h-[350px] max-w-[400px] min-w-[150px] py-8 px-4 border-2 border-black rounded-lg shadow-2xl relative">
-          <img src={currentmember.image} className="rounded-full object-fill select-none border-2 border-black h-40 w-40"/>
-          <h1 className="select-none">{currentmember.name}</h1>
-          <h1 className="select-none">{currentmember.role}</h1>
-          <FaArrowLeft
-            className="border-1 border-black rounded-full absolute top-1/2 -translate-y-1/2 left-0 cursor-pointer"
-            onClick={prevbtn}
-            size={30}
-          />
-          <FaArrowRight
-            className="border-1 border-black rounded-full absolute top-1/2 -translate-y-1/2 right-0 cursor-pointer"
-            onClick={nextbtn}
-            size={30}
-          />
-        </div>
-      </div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+            speed: 650,
+          },
+        }}
+        pagination={{
+          type: "progressbar",
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper w-[50%] mb-20 h-auto py-10 px-5 mx-auto"
+      >
+        {teamMembers.map((member, index) => (
+          <SwiperSlide
+            key={index}
+            className="flex justify-center w-full items-center h-full"
+          >
+            {/* Team Member Card Structure */}
+            <div className="flex flex-col justify-center gap-3 items-center text-2xl font-medium  h-[350px] w-full py-8 px-4 border-2 border-black rounded-lg shadow-2xl relative">
+              <img
+                src={member.image}
+                className="rounded-full object-cover select-none border-2 border-black h-40 w-40"
+                alt={member.name}
+              />
+              <h1 className="select-none text-xl font-bold">{member.name}</h1>
+              <h1 className="select-none text-base text-gray-600">
+                {member.role}
+              </h1>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
