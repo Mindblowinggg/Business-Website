ce7e83139f289cb830cafdafebb7bed5fa631d63
Mindblowinggg <akahar36@gmail.com>
2025-11-08 13:02:18 +0530

    sd

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -2,12 +2,6 @@ import React from "react";
 import image1 from "/assets/portfolio img1.jpg";
 import image2 from "/assets/portfolio img2.jpg";
 import image3 from "/assets/portfolio img3.jpg";
-import image4 from "/assets/portfolio img4.jpg";
-import image5 from "/assets/portfolio img5.jpg";
-import image6 from "/assets/portfolio img6.jpg";
-import image7 from "/assets/portfolio img7.jpg";
-import image8 from "/assets/portfolio img8.jpg";
-
 
 const PortfolioPage = () => {
   return (
@@ -22,45 +16,44 @@ const PortfolioPage = () => {
         Lorem ipsum, dolor sit amet consectetur <br /> Suscipit nemo hic quos,
         ab,
       </p>
-      <div className="grid  grid-cols-1 md:grid-cols-2 md:gap-15 gap-10  px-5 pb-3 pt-3 sm:py-5 sm:px-14">
+      <div className="grid  grid-cols-1 md:grid-cols-3 md:gap-15 gap-10  px-5 pb-3 pt-3 sm:py-5 sm:px-14">
         {/* Row 1 */}
-        <div className=" border border-gray-300 rounded h-[200px] sm:h-[300px]  lg:h-[340px]">
+        <div className="cursor-pointer border border-gray-300 rounded h-[200px] sm:h-[300px]  lg:h-[340px]">
           <img src={image1} className="w-full h-full object-cover" />
         </div>
-        <div className="  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
+        <div className="cursor-pointer  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
           <img src={image2} className="w-full h-full object-cover" />
         </div>
 
         {/* Row 2 */}
-        <div className="  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
+        <div className="cursor-pointer  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
           <img src={image3} className="w-full h-full object-cover" />
         </div>
-        <div className=" border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
-          <img src={image4} className="w-full h-full object-cover" />
+        <div className="cursor-pointer border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
+          <img src={image1} className="w-full h-full object-cover" />
         </div>
 
         {/* Row 3 */}
-        <div className="  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
-          <img src={image5} className="w-full h-full object-cover" />
+        <div className="cursor-pointer  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
+          <img src={image2} className="w-full h-full object-cover" />
         </div>
-        <div className=" border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
-          <img src={image6} className="w-full h-full object-cover" />
+        <div className="cursor-pointer border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
+          <img src={image3} className="w-full h-full object-cover" />
         </div>
 
         {/* Row 4 */}
-        <div className="  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
-          <img src={image7} className="w-full h-full object-cover" />
+        <div className="cursor-pointer  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
+          <img src={image1} className="w-full h-full object-cover" />
         </div>
-        <div className="  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
-          <img src={image8} className="w-full h-full object-cover" />
+        <div className="cursor-pointer  border border-gray-300 rounded flex items-center justify-center h-[200px] sm:h-[300px]  lg:h-[340px]">
+          <img src={image2} className="w-full h-full object-cover" />
         </div>
-        
       </div>
       <div className="learnmore pb-15  flex justify-center items-center mb-30 sm:mb-10 sm:mt-8">
-          <button className="bg-black text-white px-8 py-3 cursor-pointer font-semibold rounded-lg text-lg  transition duration-300 hover:bg-gray-600 ">
-            Learn More
-          </button>
-        </div>
+        <button className="bg-black text-white px-8 py-3 cursor-pointer font-semibold rounded-lg text-lg  transition duration-300 hover:bg-gray-600 ">
+          Learn More
+        </button>
+      </div>
     </div>
   );
 };
